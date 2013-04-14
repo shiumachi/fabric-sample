596d4ff01d5ad0192c28ff271d30dbd4b0affeef
Sho Shimauchi <sho.shimauchi@gmail.com>
2013-04-14 19:35:51 +0900

    add comment for sample codes

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
 from fabric.api import sudo
+
+
+#
+# 「今日からすぐに使えるデプロイ・システム管理ツール Fabric 入門」用サンプルコード
+# http://d.hatena.ne.jp/shiumachi/20130414/1365920515
+#
 
 
 def install_git():
